720c57f5887aaaad16c7468781b38fc0a2584bb5
Michael Bladowski <info@macropage.de>
2014-12-05 23:56:09 +0100

    shit typo

--- src/Toper/Exception/ServerErrorException.php
+++ src/Toper/Exception/ServerErrorException.php
@@ -5,17 +5,17 @@ namespace Toper\Exception;
 class ServerErrorException extends RequestException
 {
     /**
-     * @var \Guzzle\Http\Message\Respons $response
+     * @var \Guzzle\Http\Message\Response $response
      */
     private $response;
 
     /**
-     * @param \Guzzle\Http\Message\Respons $response
+     * @param \Guzzle\Http\Message\Response $response
      * @param int $message
      * @param int $code
      * @param $previous
      */
-    public function __construct(\Guzzle\Http\Message\Respons $response, $message, $code, $previous)
+    public function __construct(\Guzzle\Http\Message\Response $response, $message, $code, $previous)
     {
         parent::__construct($message, $code, $previous);
 
